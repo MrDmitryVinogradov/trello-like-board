649fcebe2260c4972154a02a2281a404e1273532
Dmitry Vinogradov <mr.dmitryvinogradov@gmail.com>
2022-03-15 14:47:30 +0300

    Field render

---
+++ src/js/Board.js
@@ -0,0 +1,97 @@
+/* eslint-disable no-param-reassign */
+export default class Board {
+  constructor() {
+    this.columns = [];
+    this.columnsText = ['TODO', 'IN PROGRESS', 'DONE'];
+    this.dragging = null;
+  }
+
+  renderField() {
+    this.field = document.createElement('div');
+    this.field.classList.add('field');
+    for (let i = 0; i < 3; i += 1) {
+      const column = document.createElement('div');
+      column.classList.add('column');
+      const header = document.createElement('div');
+      header.classList.add('column-header');
+      header.innerText = this.columnsText[i];
+      column.appendChild(header);
+      const tasksList = document.createElement('ul');
+      tasksList.classList.add('tasks-list');
+      column.appendChild(tasksList);
+      const addTask = document.createElement('div');
+      addTask.innerHTML = '<div class = \'add-task\'> Add a card </div>';
+      column.appendChild(addTask);
+      this.field.appendChild(column);
+      column.querySelector('.add-task').addEventListener('click', () => {
+        this.edit(column);
+      });
+      this.columns.push(column);
+    }
+    document.querySelector('body').appendChild(this.field);
+    this.field.addEventListener('mousedown', (evt) => {
+      if (evt.target.classList.contains('task-card')) {
+        this.dragging = evt.target.closest('li');
+      }
+    });
+    this.field.addEventListener('mousemove', (evt) => {
+      evt.preventDefault();
+      if (this.dragging) {
+        this.dragging.classList.add('moved');
+      }
+    });
+    this.columns.forEach((element) => {
+      element.addEventListener('mouseup', () => {
+        if (this.dragging) {
+          element.querySelector('.tasks-list').appendChild(this.dragging);
+          this.dragging.classList.remove('moved');
+          this.dragging = null;
+        }
+      });
+    });
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  renderEditor() {
+    const editor = document.createElement('div');
+    editor.classList.add('editor-wrapper');
+    editor.innerHTML = '<form class = \'editor\'> <textarea class = \'task-editor\' placeholder = \'What is the task? \'></textarea > <div class= \'add-task-group\'> <button class = \'add-task-btn\'> Add task </button> <div class = \'close-editor\'></div> </div> </form>';
+    return editor;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  edit(column) {
+    column.querySelector('.add-task').style.display = 'none';
+    const editor = this.renderEditor();
+    column.appendChild(editor);
+    column.querySelector('.editor').addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      this.createCard(column);
+      editor.remove();
+    });
+    column.querySelector('.close-editor').addEventListener('click', () => {
+      editor.remove();
+      column.querySelector('.add-task').style.display = 'block';
+    });
+  }
+
+  createCard(column) {
+    if (column.querySelector('.task-editor').value) {
+      const editor = this.renderEditor();
+      const card = document.createElement('li');
+      card.classList.add('task');
+      card.innerHTML = '<div class = \'task-card\' draggable = \'true\'> </div>';
+      const taskText = column.querySelector('.task-editor').value;
+      card.querySelector('.task-card').innerText = taskText;
+      const removeCard = document.createElement('div');
+      removeCard.classList.add('remove-card');
+      removeCard.addEventListener('click', () => {
+        card.remove();
+      });
+      card.querySelector('.task-card').appendChild(removeCard);
+      column.querySelector('.tasks-list').appendChild(card);
+      editor.remove();
+      column.querySelector('.add-task').style.display = 'block';
+    }
+  }
+}
---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line no-console
+import Board from './Board';
+
+const app = new Board();
+
+app.renderField();
